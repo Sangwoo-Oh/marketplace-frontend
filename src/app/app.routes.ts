5d2ae75deaf06a498a6240e4e5282df70dd99956
Sangwoo-Oh <sangwoooh1013@gmail.com>
2024-06-23 15:08:04 +0200

    auth components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,13 @@ import { Routes } from '@angular/router';
 import { TopPageComponent } from './top-page/top-page.component';
 import { CategoryComponent } from './category/category.component';
 import { DetailComponent } from './detail/detail.component';
+import { SignUpComponent } from './auth/sign-up/sign-up.component';
+import { SignInComponent } from './auth/sign-in/sign-in.component';
 
 export const routes: Routes = [
     { path: '', component: TopPageComponent },
     { path: 'cat', component: CategoryComponent },
     { path: 'itm/:id', component: DetailComponent },
+    { path: 'sign-in', component: SignInComponent },
+    { path: 'sign-up', component: SignUpComponent },
 ];
