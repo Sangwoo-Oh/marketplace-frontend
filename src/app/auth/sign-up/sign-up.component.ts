20892c7e7112a9a7b9bcb850260e7e9bac1ee1bc
Sangwoo-Oh <sangwoooh1013@gmail.com>
2024-06-23 18:39:32 +0200

    WIP: signup ngform

--- src/app/auth/sign-up/sign-up.component.ts
+++ src/app/auth/sign-up/sign-up.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { User } from '../user';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,FormsModule],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.scss'
 })
 export class SignUpComponent {
-
+  model = {
+    username:"",
+    email:"",
+    password:"",
+    password_confirm:"",
+  }
+  submitted = false;
+  onSubmit() {
+    this.submitted = true;
+  }
 }
